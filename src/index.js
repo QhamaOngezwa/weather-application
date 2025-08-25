b5dde4d209670adaf00abdb0e5ae245c971e15e1
QhamaOngezwa <qhamahani@gmail.com>
2025-08-25 15:37:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+function updateWeather(response) {
+  console.log(response.data);
+}
 function searchCity(city) {
   let apiKey = "c0fete58adfd83d2733e0c0bo44abfc6";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units = metric`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function changeCity(event) {

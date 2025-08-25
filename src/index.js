f199b2ad078b934f690f19dbc2d730c5aab94038
QhamaOngezwa <qhamahani@gmail.com>
2025-08-25 14:30:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-let city = document.querySelector("search-form-input");
-let h1 = document.querySelector("weather-app-city");
+let city = document.querySelector(".search-form-input");
+let h1 = document.querySelector(".weather-app-city");
 h1.innerHTML = city.value;

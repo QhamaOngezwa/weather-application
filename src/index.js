91133423aebb5c39c21334e1f9fd260384f8ed8e
QhamaOngezwa <qhamahani@gmail.com>
2025-08-25 14:41:34 +0200

    added id

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-let city = document.querySelector(".search-form-input");
+function changeCity(event) {
+  event.preventDefault();
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", changeCity);
 let h1 = document.querySelector(".weather-app-city");
 h1.innerHTML = city.value;

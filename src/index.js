cac5ddd49f7e22a9c26fbdc4c3206d465314185d
QhamaOngezwa <qhamahani@gmail.com>
2025-08-25 15:23:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+function searchCity(city) {
+  let apiKey = c0fete58adfd83d2733e0c0bo44abfc6;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units = metric`;
+  console.log(apiUrl);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityName = document.querySelector("#city-name");
   cityName.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", changeCity);
 
-let apiKey = c0fete58adfd83d2733e0c0bo44abfc6;
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}";
 axios.get(apiUrl);
